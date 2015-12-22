1e442a6c92e48da215a527d31892578d0273d354
Adnan Begovic <adnan@cyngn.com>
2015-12-22 11:04:08 -0800

    cmsdk: Fixup stub generation.
    Change-Id: I1b033ff646dbbdafa5d0efcaf110b76ca48980e3

--- services/java/com/evervolv/platform/internal/PerformanceManagerService.java
+++ services/java/com/evervolv/platform/internal/PerformanceManagerService.java
@@ -44,6 +44,7 @@ import evervolv.provider.EVSettings;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.regex.Pattern;
 
+/** @hide */
 public class PerformanceManagerService extends SystemService {
 
     private static final String TAG = "PerformanceManager";
