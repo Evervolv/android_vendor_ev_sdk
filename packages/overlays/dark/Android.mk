9f1b516a39d731a1f9bb4439737866740d65923a
Joey <joey@lineageos.org>
2018-05-06 13:50:32 +0200

    overlays: do not include sysuidark when our dark package is being shipped
    Change-Id: I566cd1bb1c3543b0b2a2002f5b12e38ed75554e0
    Signed-off-by: Joey <joey@lineageos.org>

--- packages/overlays/dark/Android.mk
+++ packages/overlays/dark/Android.mk
@@ -5,5 +5,6 @@ LOCAL_RRO_THEME := PlatformDarkTheme
 LOCAL_CERTIFICATE := platform
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := PlatformDarkTheme
+LOCAL_OVERRIDES_PACKAGES := SysuiDarkThemeOverlay
 
 include $(BUILD_RRO_SYSTEM_PACKAGE)
