45df9869237de0cfab00dcff1af03947e9d65ca7
Wesley.CW Wang <wesleycwwang@google.com>
2018-05-28 16:39:27 +0800

    sdk: Add emergency dialer option on power menu
    Change-Id: Iddc78758409c65ae310b79492137bf47a8ebea17

--- core/java/com/evervolv/internal/util/PowerMenuConstants.java
+++ core/java/com/evervolv/internal/util/PowerMenuConstants.java
@@ -31,6 +31,7 @@ public class PowerMenuConstants {
     public static final String GLOBAL_ACTION_KEY_VOICEASSIST = "voiceassist";
     public static final String GLOBAL_ACTION_KEY_ASSIST = "assist";
     public static final String GLOBAL_ACTION_KEY_LOGOUT = "logout";
+    public static final String GLOBAL_ACTION_KEY_EMERGENCY = "emergency";
 
     /**
      * Advanced restart menu actions
