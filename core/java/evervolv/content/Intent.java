CyanogenMod/cm_platform_sdk

cmsdk: Finish migrating "last app" logic to cmsdk.
Change-Id: I94dbd41639c734f53000b975685bf42d046a7e84

src/java/cyanogenmod/content/Intent.java
@@ -29,5 +29,15 @@ public class Intent {
      * <p>Output: Nothing
      * @hide
      */
-    public static final String ACTION_RECENTS_LONG_PRESS = "android.intent.action.RECENTS_LONG_PRESS";
+    public static final String ACTION_RECENTS_LONG_PRESS =
+            "cyanogenmod.intent.action.RECENTS_LONG_PRESS";
+
+    /**
+     * This field is part of the intent {@link #ACTION_RECENTS_LONG_PRESS}.
+     * The type of the extra is a boolean that indicates if the long press
+     * is released.
+     * @hide
+     */
+    public static final String EXTRA_RECENTS_LONG_PRESS_RELEASE =
+            "cyanogenmod.intent.extra.RECENTS_LONG_PRESS_RELEASE";
 }

Evervolv/android_vendor_ev_sdk

core/java/evervolv/content/Intent.java
@@ -29,5 +29,15 @@ public class Intent {
      * <p>Output: Nothing
      * @hide
      */
-    public static final String ACTION_RECENTS_LONG_PRESS = "android.intent.action.RECENTS_LONG_PRESS";
+    public static final String ACTION_RECENTS_LONG_PRESS =
+            "evervolv.intent.action.RECENTS_LONG_PRESS";
+
+    /**
+     * This field is part of the intent {@link #ACTION_RECENTS_LONG_PRESS}.
+     * The type of the extra is a boolean that indicates if the long press
+     * is released.
+     * @hide
+     */
+    public static final String EXTRA_RECENTS_LONG_PRESS_RELEASE =
+            "evervolv.intent.extra.RECENTS_LONG_PRESS_RELEASE";
 }