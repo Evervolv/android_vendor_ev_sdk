CyanogenMod/cm_platform_sdk

cmsdk: Change how parts are launched
 * Encode the part into the action and use an explicit intent instead
   of relying on extras.

Change-Id: I4f035d6b839e9701e20a9b78dbe2a39757aad13e

sdk/src/java/org/cyanogenmod/internal/cmparts/PartInfo.java
@@ -15,6 +15,8 @@
  */
 package org.cyanogenmod.internal.cmparts;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -125,6 +127,18 @@ public class PartInfo implements Parcelable {
         out.writeInt(mAvailable ? 1 : 0);
 
         parcelInfo.complete();
+    }
+
+    public String getAction() {
+        return PartsList.PARTS_ACTION_PREFIX + "." + mName;
+    }
+
+    public Intent getIntentForActivity() {
+        Intent i = new Intent(getAction());
+        ComponentName cn = new ComponentName(PartsList.CMPARTS_PACKAGE,
+                PartsList.CMPARTS_PACKAGE + ".PartsActivity");
+        i.setComponent(cn);
+        return i;
     }
 
     public static final Parcelable.Creator<PartInfo> CREATOR =

sdk/src/java/org/cyanogenmod/internal/cmparts/PartsList.java
@@ -44,13 +44,13 @@ import static com.android.internal.R.styleable.Preference_title;
 
 public class PartsList {
 
-    public static final String ACTION_PART = "org.cyanogenmod.cmparts.PART";
     public static final String ACTION_PART_CHANGED = "org.cyanogenmod.cmparts.PART_CHANGED";
 
     public static final String EXTRA_PART = "part";
     public static final String EXTRA_PART_KEY = "key";
 
     public static final String CMPARTS_PACKAGE = "org.cyanogenmod.cmparts";
+    public static final String PARTS_ACTION_PREFIX = CMPARTS_PACKAGE + ".parts";
 
     private static final Map<String, PartInfo> sParts = new ArrayMap<String, PartInfo>();
 

Evervolv/android_vendor_ev_sdk

core/java/com/evervolv/internal/parts/PartInfo.java
@@ -15,6 +15,8 @@
  */
 package com.evervolv.internal.parts;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -125,6 +127,18 @@ public class PartInfo implements Parcelable {
         out.writeInt(mAvailable ? 1 : 0);
 
         parcelInfo.complete();
+    }
+
+    public String getAction() {
+        return PartsList.PARTS_ACTION_PREFIX + "." + mName;
+    }
+
+    public Intent getIntentForActivity() {
+        Intent i = new Intent(getAction());
+        ComponentName cn = new ComponentName(PartsList.PARTS_PACKAGE,
+                PartsList.PARTS_PACKAGE + ".PartsActivity");
+        i.setComponent(cn);
+        return i;
     }
 
     public static final Parcelable.Creator<PartInfo> CREATOR =

core/java/com/evervolv/internal/parts/PartsList.java
@@ -43,13 +43,13 @@ import static com.android.internal.R.styleable.Preference_title;
 
 public class PartsList {
 
-    public static final String ACTION_PART = "com.evervolv.parts.PART";
     public static final String ACTION_PART_CHANGED = "com.evervolv.parts.PART_CHANGED";
 
     public static final String EXTRA_PART = "part";
     public static final String EXTRA_PART_KEY = "key";
 
     public static final String PARTS_PACKAGE = "com.evervolv.toolbox";
+    public static final String PARTS_ACTION_PREFIX = PARTS_PACKAGE + ".parts";
 
     private static final Map<String, PartInfo> sParts = new ArrayMap<String, PartInfo>();
 