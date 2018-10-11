CyanogenMod/cm_platform_sdk

cmsdk: add common action for launching camera from screen gesture
Ref: OPO-427

Change-Id: If97b2ebc864687a53c4ef8253f5d2d0bafc1e996
Signed-off-by: Roman Birg <roman@cyngn.com>

src/java/cyanogenmod/content/Intent.java
@@ -17,6 +17,8 @@
 
 package cyanogenmod.content;
 
+import android.Manifest;
+
 /**
  * CyanogenMod specific intent definition class.
  */
@@ -65,4 +67,13 @@ public class Intent {
      */
     public static final String EXTRA_PROTECTED_COMPONENTS =
             "cyanogenmod.intent.extra.PACKAGE_PROTECTED_COMPONENTS";
+
+    /**
+     * Broadcast action: notify the system that the user has performed a gesture on the screen
+     * to launch the camera. Broadcast should be protected to receivers holding the
+     * {@link Manifest.permission#STATUS_BAR_SERVICE} permission
+     */
+    public static final String ACTION_SCREEN_CAMERA_GESTURE =
+            "cyanogenmod.intent.action.SCREEN_CAMERA_GESTURE";
+
 }

Evervolv/android_vendor_ev_sdk

core/java/evervolv/content/Intent.java
@@ -17,6 +17,8 @@
 
 package evervolv.content;
 
+import android.Manifest;
+
 /**
  * Evervolv specific intent definition class.
  */
@@ -40,4 +42,13 @@ public class Intent {
      */
     public static final String EXTRA_RECENTS_LONG_PRESS_RELEASE =
             "evervolv.intent.extra.RECENTS_LONG_PRESS_RELEASE";
+
+    /**
+     * Broadcast action: notify the system that the user has performed a gesture on the screen
+     * to launch the camera. Broadcast should be protected to receivers holding the
+     * {@link Manifest.permission#STATUS_BAR_SERVICE} permission.
+     * @hide
+     */
+    public static final String ACTION_SCREEN_CAMERA_GESTURE =
+            "evervolv.intent.action.SCREEN_CAMERA_GESTURE";
 }