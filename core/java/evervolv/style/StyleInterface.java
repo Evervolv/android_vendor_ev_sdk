LineageOS/android_lineage-sdk

styles: add support for more dark overlays
Change-Id: If9e08ba8ff28cff7a4061a6b6cea10d5fe38a541
Signed-off-by: Joey <joey@lineageos.org>

sdk/src/java/lineageos/style/StyleInterface.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, The CyanogenMod Project
+ * Copyright (c) 2018, The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,6 +70,22 @@ public class StyleInterface {
      * @see #setAccent
      */
     public static final String ACCENT_DEFAULT = "lineageos";
+
+    /**
+     * Dark style: default
+     *
+     * @see #setDarkOverlay
+     * @hide
+     */
+    public static final String OVERLAY_DARK_DEFAULT = "org.lineageos.overlay.dark";
+
+    /**
+     * Dark style: black
+     *
+     * @see #setDarkOverlay
+     * @hide
+     */
+    public static final String OVERLAY_DARK_BLACK = "org.lineageos.overlay.black";
 
     /**
      * Allows an application to change system style.
@@ -267,5 +283,70 @@ public class StyleInterface {
             Log.e(TAG, e.getLocalizedMessage(), e);
         }
         return fallback;
+    }
+
+    /**
+     * Determine whether the dark style is being used right now,
+     * regardless of the current global style mode.
+     *
+     * @return Returns true if dark style is enabled
+     */
+    public boolean isDarkNow() {
+        if (sService == null) {
+            return false;
+        }
+        try {
+            return sService.isDarkNow();
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getLocalizedMessage(), e);
+        }
+        return false;
+    }
+
+    /**
+     * Set dark overlay package.
+     *
+     * You will need {@link #CHANGE_STYLE_SETTINGS_PERMISSION}
+     * to utilize this functionality.
+     *
+     * @see #OVERLAY_DARK_DEFAULT
+     * @see #OVERLAY_DARK_BLACK
+     * @param overlayName The package name of the overlay.
+     *             One of {@link #OVERLAY_DARK_DEFAULT} or
+     *             {@link #OVERLAY_DARK_BLACK},
+     *
+     * @return Whether the process failed
+     * @hide
+     */
+    public boolean setDarkOverlay(String overlayName) {
+        if (sService == null) {
+            return false;
+        }
+        try {
+            return sService.setDarkOverlay(overlayName);
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getLocalizedMessage(), e);
+        }
+        return false;
+    }
+
+    /**
+     * Get current dark overlay package name.
+     *
+     * @see #OVERLAY_DARK_DEFAULT
+     * @see #OVERLAY_DARK_BLACK
+     * @@return {@link #OVERLAY_DARK_DEFAULT} or {@link #OVERLAY_DARK_BLACK}
+     * @hide
+     */
+    public String getDarkOverlay() {
+        if (sService == null) {
+            return OVERLAY_DARK_DEFAULT;
+        }
+        try {
+            return sService.getDarkOverlay();
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getLocalizedMessage(), e);
+        }
+        return OVERLAY_DARK_DEFAULT;
     }
 }

lineage/lib/main/java/org/lineageos/platform/internal/StyleInterfaceService.java
@@ -79,6 +79,8 @@ public class StyleInterfaceService extends LineageSystemService {
                 "You do not have permissions to change system style");
     }
 
+    /* Public methods implementation */
+
     private boolean setGlobalStyleInternal(int mode, String packageName) {
         // Check whether the packageName is valid
         if (isAValidPackage(packageName)) {
@@ -168,6 +170,44 @@ public class StyleInterfaceService extends LineageSystemService {
         return results;
     }
 
+    private boolean isDarkNowInternal() {
+        String target = getDarkOverlayInternal();
+        return isEnabled(target);
+    }
+
+    private boolean setDarkOverlayInternal(String overlayName) {
+        boolean isDefault = StyleInterface.OVERLAY_DARK_DEFAULT.equals(overlayName);
+        boolean isBlack = StyleInterface.OVERLAY_DARK_BLACK.equals(overlayName);
+        int userId = UserHandle.myUserId();
+
+        if (!isDefault && !isBlack) {
+            Log.e(TAG, overlayName + " is not a valid dark overlay!");
+            return false;
+        }
+
+        try {
+            String currentDarkOverlay = LineageSettings.System.getString(
+                    mContext.getContentResolver(), LineageSettings.System.BERRY_DARK_OVERLAY,
+                    StyleInterface.OVERLAY_DARK_DEFAULT);
+            if (isEnabled(currentDarkOverlay)) {
+                // Swich dark overlays
+                mOverlayService.setEnabled(currentDarkOverlay, false, userId);
+                mOverlayService.setEnabled(overlayName, true, userId);
+            }
+            return true;
+        } catch (RemoteException e) {
+            Log.e(TAG, "Failed to change dark overlay");
+        }
+        return false;
+    }
+
+    private String getDarkOverlayInternal() {
+        return LineageSettings.System.getString(mContext.getContentResolver(),
+                LineageSettings.System.BERRY_DARK_OVERLAY, StyleInterface.OVERLAY_DARK_DEFAULT);
+    }
+
+    /* Utils */
+
     private int getBestColor(int sourceColor, int[] colors) {
         int best = 0;
         double minDiff = Double.MAX_VALUE;
@@ -205,6 +245,17 @@ public class StyleInterfaceService extends LineageSystemService {
         }
     }
 
+    private boolean isEnabled(String pkgName) {
+        int userId = UserHandle.myUserId();
+        try {
+            OverlayInfo info = mOverlayService.getOverlayInfo(pkgName, userId);
+            return info.isEnabled();
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+        }
+        return false;
+    }
+
     private boolean isValidAccent(String pkgName) {
         try {
             ApplicationInfo ai = mPackageManager.getApplicationInfo(pkgName,
@@ -224,6 +275,8 @@ public class StyleInterfaceService extends LineageSystemService {
             return false;
         }
     }
+
+    /* Binder */
 
     private final IBinder mService = new IStyleInterface.Stub() {
         @Override
@@ -306,6 +359,46 @@ public class StyleInterfaceService extends LineageSystemService {
              */
             long token = clearCallingIdentity();
             List<String> result = getTrustedAccentsInternal();
+            restoreCallingIdentity(token);
+            return result;
+        }
+
+        @Override
+        public boolean isDarkNow() {
+            /*
+             * We need to clear the caller's identity in order to
+             *   allow this method call to modify settings
+             *   not allowed by the caller's permissions.
+             */
+            long token = clearCallingIdentity();
+            boolean result = isDarkNowInternal();
+            restoreCallingIdentity(token);
+            return result;
+        }
+
+        @Override
+        public boolean setDarkOverlay(String overlayName) {
+            enforceChangeStylePermission();
+            /*
+             * We need to clear the caller's identity in order to
+             *   allow this method call to modify settings
+             *   not allowed by the caller's permissions.
+             */
+            long token = clearCallingIdentity();
+            boolean success = setDarkOverlayInternal(overlayName);
+            restoreCallingIdentity(token);
+            return success;
+        }
+
+        @Override
+        public String getDarkOverlay() {
+            /*
+             * We need to clear the caller's identity in order to
+             *   allow this method call to modify settings
+             *   not allowed by the caller's permissions.
+             */
+            long token = clearCallingIdentity();
+            String result = getDarkOverlayInternal();
             restoreCallingIdentity(token);
             return result;
         }

Evervolv/android_vendor_ev_sdk

core/java/evervolv/style/StyleInterface.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, The CyanogenMod Project
+ * Copyright (c) 2018, The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,6 +70,22 @@ public class StyleInterface {
      * @see #setAccent
      */
     public static final String ACCENT_DEFAULT = "evervolv";
+
+    /**
+     * Dark style: default
+     *
+     * @see #setDarkOverlay
+     * @hide
+     */
+    public static final String OVERLAY_DARK_DEFAULT = "com.evervolv.overlay.dark";
+
+    /**
+     * Dark style: black
+     *
+     * @see #setDarkOverlay
+     * @hide
+     */
+    public static final String OVERLAY_DARK_BLACK = "com.evervolv.overlay.black";
 
     /**
      * Allows an application to change system style.
@@ -267,5 +283,70 @@ public class StyleInterface {
             Log.e(TAG, e.getLocalizedMessage(), e);
         }
         return fallback;
+    }
+
+    /**
+     * Determine whether the dark style is being used right now,
+     * regardless of the current global style mode.
+     *
+     * @return Returns true if dark style is enabled
+     */
+    public boolean isDarkNow() {
+        if (sService == null) {
+            return false;
+        }
+        try {
+            return sService.isDarkNow();
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getLocalizedMessage(), e);
+        }
+        return false;
+    }
+
+    /**
+     * Set dark overlay package.
+     *
+     * You will need {@link #CHANGE_STYLE_SETTINGS_PERMISSION}
+     * to utilize this functionality.
+     *
+     * @see #OVERLAY_DARK_DEFAULT
+     * @see #OVERLAY_DARK_BLACK
+     * @param overlayName The package name of the overlay.
+     *             One of {@link #OVERLAY_DARK_DEFAULT} or
+     *             {@link #OVERLAY_DARK_BLACK},
+     *
+     * @return Whether the process failed
+     * @hide
+     */
+    public boolean setDarkOverlay(String overlayName) {
+        if (sService == null) {
+            return false;
+        }
+        try {
+            return sService.setDarkOverlay(overlayName);
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getLocalizedMessage(), e);
+        }
+        return false;
+    }
+
+    /**
+     * Get current dark overlay package name.
+     *
+     * @see #OVERLAY_DARK_DEFAULT
+     * @see #OVERLAY_DARK_BLACK
+     * @@return {@link #OVERLAY_DARK_DEFAULT} or {@link #OVERLAY_DARK_BLACK}
+     * @hide
+     */
+    public String getDarkOverlay() {
+        if (sService == null) {
+            return OVERLAY_DARK_DEFAULT;
+        }
+        try {
+            return sService.getDarkOverlay();
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getLocalizedMessage(), e);
+        }
+        return OVERLAY_DARK_DEFAULT;
     }
 }

services/java/com/evervolv/platform/internal/StyleInterfaceService.java
@@ -79,6 +79,8 @@ public class StyleInterfaceService extends VendorService {
                 "You do not have permissions to change system style");
     }
 
+    /* Public methods implementation */
+
     private boolean setGlobalStyleInternal(int mode, String packageName) {
         // Check whether the packageName is valid
         if (isAValidPackage(packageName)) {
@@ -168,6 +170,44 @@ public class StyleInterfaceService extends VendorService {
         return results;
     }
 
+    private boolean isDarkNowInternal() {
+        String target = getDarkOverlayInternal();
+        return isEnabled(target);
+    }
+
+    private boolean setDarkOverlayInternal(String overlayName) {
+        boolean isDefault = StyleInterface.OVERLAY_DARK_DEFAULT.equals(overlayName);
+        boolean isBlack = StyleInterface.OVERLAY_DARK_BLACK.equals(overlayName);
+        int userId = UserHandle.myUserId();
+
+        if (!isDefault && !isBlack) {
+            Log.e(TAG, overlayName + " is not a valid dark overlay!");
+            return false;
+        }
+
+        try {
+            String currentDarkOverlay = EVSettings.System.getString(
+                    mContext.getContentResolver(), EVSettings.System.BERRY_DARK_OVERLAY,
+                    StyleInterface.OVERLAY_DARK_DEFAULT);
+            if (isEnabled(currentDarkOverlay)) {
+                // Swich dark overlays
+                mOverlayService.setEnabled(currentDarkOverlay, false, userId);
+                mOverlayService.setEnabled(overlayName, true, userId);
+            }
+            return true;
+        } catch (RemoteException e) {
+            Log.e(TAG, "Failed to change dark overlay");
+        }
+        return false;
+    }
+
+    private String getDarkOverlayInternal() {
+        return EVSettings.System.getString(mContext.getContentResolver(),
+                EVSettings.System.BERRY_DARK_OVERLAY, StyleInterface.OVERLAY_DARK_DEFAULT);
+    }
+
+    /* Utils */
+
     private int getBestColor(int sourceColor, int[] colors) {
         int best = 0;
         double minDiff = Double.MAX_VALUE;
@@ -205,6 +245,17 @@ public class StyleInterfaceService extends VendorService {
         }
     }
 
+    private boolean isEnabled(String pkgName) {
+        int userId = UserHandle.myUserId();
+        try {
+            OverlayInfo info = mOverlayService.getOverlayInfo(pkgName, userId);
+            return info.isEnabled();
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+        }
+        return false;
+    }
+
     private boolean isValidAccent(String pkgName) {
         try {
             ApplicationInfo ai = mPackageManager.getApplicationInfo(pkgName,
@@ -224,6 +275,8 @@ public class StyleInterfaceService extends VendorService {
             return false;
         }
     }
+
+    /* Binder */
 
     private final IBinder mService = new IStyleInterface.Stub() {
         @Override
@@ -306,6 +359,46 @@ public class StyleInterfaceService extends VendorService {
              */
             long token = clearCallingIdentity();
             List<String> result = getTrustedAccentsInternal();
+            restoreCallingIdentity(token);
+            return result;
+        }
+
+        @Override
+        public boolean isDarkNow() {
+            /*
+             * We need to clear the caller's identity in order to
+             *   allow this method call to modify settings
+             *   not allowed by the caller's permissions.
+             */
+            long token = clearCallingIdentity();
+            boolean result = isDarkNowInternal();
+            restoreCallingIdentity(token);
+            return result;
+        }
+
+        @Override
+        public boolean setDarkOverlay(String overlayName) {
+            enforceChangeStylePermission();
+            /*
+             * We need to clear the caller's identity in order to
+             *   allow this method call to modify settings
+             *   not allowed by the caller's permissions.
+             */
+            long token = clearCallingIdentity();
+            boolean success = setDarkOverlayInternal(overlayName);
+            restoreCallingIdentity(token);
+            return success;
+        }
+
+        @Override
+        public String getDarkOverlay() {
+            /*
+             * We need to clear the caller's identity in order to
+             *   allow this method call to modify settings
+             *   not allowed by the caller's permissions.
+             */
+            long token = clearCallingIdentity();
+            String result = getDarkOverlayInternal();
             restoreCallingIdentity(token);
             return result;
         }