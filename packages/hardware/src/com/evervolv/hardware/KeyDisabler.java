CyanogenMod/android_frameworks_opt_hardware

HAF: Add KeyDisabler stub
Allow disabling the capacitive keys on a device

Change-Id: I855cb094963bc3263fd68e4dacbbd8f331ccb825

src/org/cyanogenmod/hardware/KeyDisabler.java
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.hardware;
+
+/*
+ * Disable capacitive keys
+ *
+ * This is intended for use on devices in which the capacitive keys
+ * can be fully disabled for replacement with a soft navbar. You
+ * really should not be using this on a device with mechanical or
+ * otherwise visible-when-inactive keys
+ */
+
+public class KeyDisabler {
+
+    /*
+     * All HAF classes should export this boolean.
+     * Real implementations must, of course, return true
+     */
+
+    public static boolean isSupported() { return false; }
+
+    /*
+     * Are the keys currently blocked?
+     */
+
+    public static boolean isActive() {
+        return false;
+    }
+
+    /*
+     * Disable capacitive keys
+     */
+
+    public static boolean setActive(boolean state) {
+        throw new UnsupportedOperationException();
+    }
+
+}

Evervolv/android_vendor_ev_sdk

packages/hardware/src/com/evervolv/hardware/KeyDisabler.java
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evervolv.hardware;
+
+/*
+ * Disable capacitive keys
+ *
+ * This is intended for use on devices in which the capacitive keys
+ * can be fully disabled for replacement with a soft navbar. You
+ * really should not be using this on a device with mechanical or
+ * otherwise visible-when-inactive keys
+ */
+
+public class KeyDisabler {
+
+    /*
+     * All HAF classes should export this boolean.
+     * Real implementations must, of course, return true
+     */
+
+    public static boolean isSupported() { return false; }
+
+    /*
+     * Are the keys currently blocked?
+     */
+
+    public static boolean isActive() {
+        return false;
+    }
+
+    /*
+     * Disable capacitive keys
+     */
+
+    public static boolean setActive(boolean state) {
+        throw new UnsupportedOperationException();
+    }
+
+}