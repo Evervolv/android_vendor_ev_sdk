CyanogenMod/android_frameworks_opt_hardware

Hardware: Add high touch sensitivity support
Change-Id: Ia5ad3695ca253269dfe3a62420819ad9e9367f9c

src/org/cyanogenmod/hardware/HighTouchSensitivity.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2013 Xiao-Long Chen <chenxiaolong@cxl.epac.to>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.hardware;
+
+/**
+ * Glove mode / high touch sensitivity (on Samsung Galaxy S4)
+ */
+public class HighTouchSensitivity {
+
+    /**
+     * Whether device supports high touch sensitivity.
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() { return false; }
+
+    /**
+     * This method allows to setup high touch sensitivity status.
+     *
+     * @param status The new high touch sensitivity status
+     * @return boolean Must be false if high touch sensitivity is not supported or the operation
+     * failed; true in any other case.
+     */
+    public static boolean setEnabled(boolean status) { return false; }
+
+}

Evervolv/android_vendor_ev_sdk

packages/hardware/src/com/evervolv/hardware/HighTouchSensitivity.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2013 Xiao-Long Chen <chenxiaolong@cxl.epac.to>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evervolv.hardware;
+
+/**
+ * Glove mode / high touch sensitivity
+ */
+public class HighTouchSensitivity {
+
+    /**
+     * Whether device supports high touch sensitivity.
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() { return false; }
+
+    /**
+     * This method allows to setup high touch sensitivity status.
+     *
+     * @param status The new high touch sensitivity status
+     * @return boolean Must be false if high touch sensitivity is not supported or the operation
+     * failed; true in any other case.
+     */
+    public static boolean setEnabled(boolean status) { return false; }
+
+}