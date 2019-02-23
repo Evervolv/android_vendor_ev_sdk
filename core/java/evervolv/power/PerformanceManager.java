CyanogenMod/cm_platform_sdk

cmsdk: Mandate performance feature xml's for service implementation.
  The feature xml plays two roles:

      1) To allow sdk interface (constructor) to throw when system
      service is unavailable. This allows for clearer platform
      development debugging.

      2) To allow for simpler disambiguation of what services to
      instrument in a modular environment.

Change-Id: I50f5993ff9c5107fdeaa9a5aa95377235eb3ac02
TICKET: CYNGNOS-2294

src/java/cyanogenmod/power/PerformanceManager.java
@@ -78,7 +78,12 @@ public class PerformanceManager {
 
     private PerformanceManager(Context context) {
         sService = getService();
-
+        if (context.getPackageManager().hasSystemFeature(
+                CMContextConstants.Features.PERFORMANCE) && sService == null) {
+            throw new RuntimeException("Unable to get PerformanceManagerService. The service" +
+                    " either crashed, was not started, or the interface has been called to early" +
+                    " in SystemServer init");
+        }
         try {
             if (sService != null) {
                 mNumberOfProfiles = sService.getNumberOfProfiles();

cm/lib/main/java/org/cyanogenmod/platform/internal/PerformanceManagerService.java
@@ -29,6 +29,7 @@ import android.os.Message;
 import android.os.Process;
 import android.os.SystemProperties;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Slog;
 
 import com.android.server.ServiceThread;
@@ -110,7 +111,13 @@ public class PerformanceManagerService extends SystemService {
 
     @Override
     public void onStart() {
-        publishBinderService(CMContextConstants.CM_PERFORMANCE_SERVICE, mBinder);
+        if (mContext.getPackageManager().hasSystemFeature(
+                CMContextConstants.Features.PERFORMANCE)) {
+            publishBinderService(CMContextConstants.CM_PERFORMANCE_SERVICE, mBinder);
+        } else {
+            Log.wtf(TAG, "CM performance service started by system server but feature xml not" +
+                    " declared. Not publishing binder service!");
+        }
         publishLocalService(PerformanceManagerInternal.class, new LocalService());
     }
 

Evervolv/android_vendor_ev_sdk

core/java/evervolv/power/PerformanceManager.java
@@ -78,7 +78,12 @@ public class PerformanceManager {
 
     private PerformanceManager(Context context) {
         sService = getService();
-
+        if (context.getPackageManager().hasSystemFeature(
+                ContextConstants.Features.PERFORMANCE) && sService == null) {
+            throw new RuntimeException("Unable to get PerformanceManagerService. The service" +
+                    " either crashed, was not started, or the interface has been called to early" +
+                    " in SystemServer init");
+        }
         try {
             if (sService != null) {
                 mNumberOfProfiles = sService.getNumberOfProfiles();

services/java/com/evervolv/platform/internal/PerformanceManagerService.java
@@ -29,6 +29,7 @@ import android.os.Message;
 import android.os.Process;
 import android.os.SystemProperties;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Slog;
 
 import com.android.server.ServiceThread;
@@ -110,7 +111,13 @@ public class PerformanceManagerService extends SystemService {
 
     @Override
     public void onStart() {
-        publishBinderService(ContextConstants.PERFORMANCE_SERVICE, mBinder);
+        if (mContext.getPackageManager().hasSystemFeature(
+                ContextConstants.Features.PERFORMANCE)) {
+            publishBinderService(ContextConstants.PERFORMANCE_SERVICE, mBinder);
+        } else {
+            Log.wtf(TAG, "Performance service started by system server but feature xml not" +
+                    " declared. Not publishing binder service!");
+        }
         publishLocalService(PerformanceManagerInternal.class, new LocalService());
     }
 