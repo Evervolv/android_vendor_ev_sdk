CyanogenMod/cm_platform_sdk

cmsdk: Refactor the PerformanceManager
 * Remove all code and configuration into CMSDK.

Change-Id: Ib14ce5b8623cb368e6b545d1f82bc9c58580e13b

src/java/cyanogenmod/power/IPerformanceManager.aidl
@@ -1,0 +1,29 @@
+/*
+ * Copyright 2015, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package cyanogenmod.power;
+
+/** @hide */
+interface IPerformanceManager {
+
+    oneway void cpuBoost(int duration);
+
+    boolean hasPowerProfiles();
+
+    boolean setPowerProfile(int profile);
+
+    int getPowerProfile();
+}

src/java/cyanogenmod/power/PerformanceManagerInternal.java
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cyanogenmod.power;
+
+import android.content.Intent;
+
+public interface PerformanceManagerInternal {
+    
+    void activityResumed(Intent intent);
+    
+    void cpuBoost(int duration);
+    
+    void launchBoost();
+}

Evervolv/android_vendor_ev_sdk

 * Deprecate the properties-based API

core/java/evervolv/power/IPerformanceManager.aidl
@@ -1,0 +1,29 @@
+/*
+ * Copyright 2015, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package evervolv.power;
+
+/** @hide */
+interface IPerformanceManager {
+
+    oneway void cpuBoost(int duration);
+
+    boolean setPowerProfile(int profile);
+
+    int getPowerProfile();
+
+    int getNumberOfProfiles();
+}

core/java/evervolv/power/PerformanceManagerInternal.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package evervolv.power;
+
+import android.content.Intent;
+
+/** {@hide} */
+public interface PerformanceManagerInternal {
+    
+    void activityResumed(Intent intent);
+    
+    void cpuBoost(int duration);
+    
+    void launchBoost();
+}