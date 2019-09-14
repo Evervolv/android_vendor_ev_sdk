LineageOS/android_lineage-sdk

lineage-sdk: Include authority in content provider calls
API changed here:
https://github.com/LineageOS/android_frameworks_base/commit/633a13e2fa8f378d523d4ae209dc573c66db8cb1#diff-944e798f679ef205433bd711345d06dcR75

Fixes:
AndroidRuntime: Caused by: java.lang.SecurityException: The authority
unknown does not match the one of the contentProvider: lineagesettings

Change-Id: I5fe85fee777f919824e6a2ff78986e2d736e0267

packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/PreBootReceiver.java
@@ -41,7 +41,7 @@ public class PreBootReceiver extends BroadcastReceiver{
                 LineageSettings.AUTHORITY);
 
         try{
-            contentProvider.call(contentResolver.getPackageName(),
+            contentProvider.call(contentResolver.getPackageName(), LineageSettings.AUTHORITY,
                     LineageSettings.CALL_METHOD_MIGRATE_SETTINGS, null, null);
         } catch (RemoteException ex) {
             Log.w(TAG, "Failed to trigger settings migration due to RemoteException");

Evervolv/android_vendor_ev_sdk

packages/SettingsProvider/src/com/evervolv/evsettings/PreBootReceiver.java
@@ -41,7 +41,7 @@ public class PreBootReceiver extends BroadcastReceiver{
                 EVSettings.AUTHORITY);
 
         try{
-            contentProvider.call(contentResolver.getPackageName(),
+            contentProvider.call(contentResolver.getPackageName(), EVSettings.AUTHORITY,
                     EVSettings.CALL_METHOD_MIGRATE_SETTINGS, null, null);
         } catch (RemoteException ex) {
             Log.w(TAG, "Failed to trigger settings migration due to RemoteException");