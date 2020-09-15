956f5faa43d384f62c15bbd9770252a1a7be4edf
LuK1337 <priv.luk@gmail.com>
2020-09-15 02:29:56 +0200

    sdk: Clear variables before attempting to generate res R/Manifest.java
    * Fixes really strange looking errors while building rild:
      FAILED: ninja: 'out/target/product/generic_arm64/vendor/bin/hw/org.lineageos.platform-res.apk', needed by 'out/target/product/generic_arm64/vendor/bin/hw/rild', missing and no known rule to make it
    
    Change-Id: I0fb91383829b9b478b3dee9686e9e719ea3ed695

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2015 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
 evervolv_stub_packages := evervolv.app:evervolv.content:evervolv.hardware:evervolv.os:evervolv.preference:evervolv.provider:evervolv.platform:evervolv.power:evervolv.util:evervolv.style
 
 evervolv_framework_module := $(LOCAL_INSTALLED_MODULE)
