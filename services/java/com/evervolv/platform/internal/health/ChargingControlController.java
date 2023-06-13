fd4d309b032e796926c9136309f622f3419ff3b8
Luofan Chen <clfbbn@gmail.com>
2023-06-13 10:53:58 +0800

    ChargingControl: Reset internal states on settings change
    Change-Id: Ib49d6b5b9190a71f5447125ed1aa70da894e6c18

--- services/java/com/evervolv/platform/internal/health/ChargingControlController.java
+++ services/java/com/evervolv/platform/internal/health/ChargingControlController.java
@@ -637,8 +637,8 @@ public class ChargingControlController extends VendorHealthFeature {
                 EVSettings.System.CHARGING_CONTROL_TARGET_TIME,
                 mDefaultTargetTime);
 
-        // Cancel notification, so that it can be updated later
-        mChargingNotification.cancel();
+        // Reset internal states
+        resetInternalState();
 
         // Update based on those values
         updateChargeControl();
