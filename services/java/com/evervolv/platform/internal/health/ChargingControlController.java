54e9f1a6f5a028c1bef5398ab052860590f162fc
LuK1337 <priv.luk@gmail.com>
2023-07-10 12:05:50 +0200

    ChargingControl: Use ServiceManager.waitForDeclaredService()
    It might take a while until health HAL is up, thus let's just wait for
    it.
    
    Change-Id: Ie929a25a2a774aedb4559662e9ad0a47f1b154e6

--- services/java/com/evervolv/platform/internal/health/ChargingControlController.java
+++ services/java/com/evervolv/platform/internal/health/ChargingControlController.java
@@ -142,7 +142,8 @@ public class ChargingControlController extends VendorHealthFeature {
 
         mContentResolver = mContext.getContentResolver();
         mChargingControl = IChargingControl.Stub.asInterface(
-                ServiceManager.getService(IChargingControl.DESCRIPTOR + "/default"));
+                ServiceManager.waitForDeclaredService(
+                        IChargingControl.DESCRIPTOR + "/default"));
 
         if (mChargingControl == null) {
             Log.i(TAG, "Health HAL not found");
