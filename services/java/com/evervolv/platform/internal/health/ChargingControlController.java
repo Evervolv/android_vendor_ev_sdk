LineageOS/android_lineage-sdk

ChargingControl: Don't set deadline if charging control is not enabled
Change-Id: I5e7570184294e8391e4908e449c1bffb07d9410d

lineage/lib/main/java/org/lineageos/platform/internal/health/ChargingControlController.java
@@ -575,15 +575,15 @@ public class ChargingControlController extends LineageHealthFeature {
             return;
         }
 
-        final ChargeTime t = getChargeTime();
-        if (t != null && t.getTargetTime() == mSavedTargetTime) {
-            return;
-        }
-
         long deadline = 0;
-        if (t == null || mIsControlCancelledOnce) {
+        final ChargeTime t = getChargeTime();
+
+        if (!mIsConfigEnabled || t == null || mIsControlCancelledOnce) {
             deadline = -1;
         } else {
+            if (t.getTargetTime() == mSavedTargetTime) {
+                return;
+            }
             mSavedTargetTime = t.getTargetTime();
             final long targetTime = t.getTargetTime();
             final long currentTime = System.currentTimeMillis();

Evervolv/android_vendor_ev_sdk

services/java/com/evervolv/platform/internal/health/ChargingControlController.java
@@ -573,15 +573,15 @@ public class ChargingControlController extends VendorHealthFeature {
             return;
         }
 
-        final ChargeTime t = getChargeTime();
-        if (t != null && t.getTargetTime() == mSavedTargetTime) {
-            return;
-        }
-
         long deadline = 0;
-        if (t == null || mIsControlCancelledOnce) {
+        final ChargeTime t = getChargeTime();
+
+        if (!mConfigEnabled || t == null || mIsControlCancelledOnce) {
             deadline = -1;
         } else {
+            if (t.getTargetTime() == mSavedTargetTime) {
+                return;
+            }
             mSavedTargetTime = t.getTargetTime();
             final long targetTime = t.getTargetTime();
             final long currentTime = System.currentTimeMillis();