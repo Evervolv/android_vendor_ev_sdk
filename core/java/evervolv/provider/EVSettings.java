99da60edeeb909c93d42ebcd8c4792851277b920
Nick Reuter <nreuter85@gmail.com>
2024-11-02 20:54:01 -0500

    Revert "LiveDisplay: add support to AntiFlicker modes"
    This reverts commit ae91c9f64ed2316bf48f8f3e91ddac57899717eb.

--- core/java/evervolv/provider/EVSettings.java
+++ core/java/evervolv/provider/EVSettings.java
@@ -1825,7 +1825,7 @@ public final class EVSettings {
 
         /** @hide */
         public static final Validator DISPLAY_ANTI_FLICKER_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 2);
+                sBooleanValidator;
 
         /**
          * The user selected preferred refresh rate in frames per second.
