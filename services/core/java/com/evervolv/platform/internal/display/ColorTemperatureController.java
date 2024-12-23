CJ001im/android_Sequoia

sdk: A few finals don't hurt
And a few language features (HashMap constructors, collapsing
catch blocks, method references)
plus two typos

Change-Id: I9ae2f7c006c5f2f9c7c99eab81059c08bece1202

lineage/lib/main/java/org/lineageos/platform/internal/display/ColorTemperatureController.java
@@ -1,12 +1,11 @@
 /*
  * SPDX-FileCopyrightText: 2016 The CyanogenMod Project
- * SPDX-FileCopyrightText: 2018-2019 The LineageOS Project
+ * SPDX-FileCopyrightText: 2018-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 package org.lineageos.platform.internal.display;
 
 import android.animation.ValueAnimator;
-import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Handler;
@@ -48,7 +47,7 @@ public class ColorTemperatureController extends LiveDisplayFeature {
     private int mDayTemperature;
     private int mNightTemperature;
 
-    private AccelerateDecelerateInterpolator mInterpolator;
+    private final AccelerateDecelerateInterpolator mInterpolator;
     private ValueAnimator mAnimator;
 
     private final LineageHardwareManager mHardware;
@@ -159,12 +158,7 @@ public class ColorTemperatureController extends LiveDisplayFeature {
         pw.println("    isTransitioning=" + isTransitioning());
     }
 
-    private final Runnable mTransitionRunnable = new Runnable() {
-        @Override
-        public void run() {
-            updateColorTemperature();
-        }
-    };
+    private final Runnable mTransitionRunnable = this::updateColorTemperature;
 
     private boolean isTransitioning() {
         return getMode() == MODE_AUTO &&
@@ -212,7 +206,7 @@ public class ColorTemperatureController extends LiveDisplayFeature {
             return;
         }
 
-        long duration = (long)(5 * Math.abs(current - balance));
+        long duration = 5L * Math.abs(current - balance);
 
 
         if (DEBUG) {
@@ -228,14 +222,11 @@ public class ColorTemperatureController extends LiveDisplayFeature {
         mAnimator = ValueAnimator.ofInt(current, balance);
         mAnimator.setDuration(duration);
         mAnimator.setInterpolator(mInterpolator);
-        mAnimator.addUpdateListener(new AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(final ValueAnimator animation) {
-                synchronized (ColorTemperatureController.this) {
-                    if (isScreenOn()) {
-                        int value = (int) animation.getAnimatedValue();
-                        mHardware.setColorBalance(value);
-                    }
+        mAnimator.addUpdateListener(animation -> {
+            synchronized (ColorTemperatureController.this) {
+                if (isScreenOn()) {
+                    int value = (int) animation.getAnimatedValue();
+                    mHardware.setColorBalance(value);
                 }
             }
         });
@@ -247,7 +238,8 @@ public class ColorTemperatureController extends LiveDisplayFeature {
      * correct configuration at the device level!
      */
     private int mapColorTemperatureToBalance(int temperature) {
-        double z = org.lineageos.internal.util.MathUtils.powerCurveToLinear(mColorBalanceCurve, temperature);
+        double z = org.lineageos.internal.util.MathUtils.powerCurveToLinear(mColorBalanceCurve,
+                temperature);
         return Math.round(MathUtils.lerp((float)mColorBalanceRange.getLower(),
                 (float)mColorBalanceRange.getUpper(), (float)z));
     }
@@ -294,12 +286,14 @@ public class ColorTemperatureController extends LiveDisplayFeature {
 
         // Scale the transition into night mode in 0.5hr before civil sunset
         if (now <= sunset) {
-            return mInterpolator.getInterpolation((float) (sunset - now) / TWILIGHT_ADJUSTMENT_TIME);
+            return mInterpolator.getInterpolation((float) (sunset - now)
+                    / TWILIGHT_ADJUSTMENT_TIME);
         }
 
         // Scale the transition into day mode in 0.5hr after civil sunrise
         if (now >= sunrise) {
-            return mInterpolator.getInterpolation((float) (now - sunrise) / TWILIGHT_ADJUSTMENT_TIME);
+            return mInterpolator.getInterpolation((float) (now - sunrise)
+                    / TWILIGHT_ADJUSTMENT_TIME);
         }
 
         // More than 0.5hr past civil sunset

Evervolv/android_vendor_ev_sdk

services/core/java/com/evervolv/platform/internal/display/ColorTemperatureController.java
@@ -1,12 +1,11 @@
 /*
  * SPDX-FileCopyrightText: 2016 The CyanogenMod Project
- * SPDX-FileCopyrightText: 2018-2019 The LineageOS Project
+ * SPDX-FileCopyrightText: 2018-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.evervolv.platform.internal.display;
 
 import android.animation.ValueAnimator;
-import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Handler;
@@ -50,7 +49,7 @@ public class ColorTemperatureController extends LiveDisplayFeature {
     private int mDayTemperature;
     private int mNightTemperature;
 
-    private AccelerateDecelerateInterpolator mInterpolator;
+    private final AccelerateDecelerateInterpolator mInterpolator;
     private ValueAnimator mAnimator;
 
     private final HardwareManager mHardware;
@@ -161,12 +160,7 @@ public class ColorTemperatureController extends LiveDisplayFeature {
         pw.println("    isTransitioning=" + isTransitioning());
     }
 
-    private final Runnable mTransitionRunnable = new Runnable() {
-        @Override
-        public void run() {
-            updateColorTemperature();
-        }
-    };
+    private final Runnable mTransitionRunnable = this::updateColorTemperature;
 
     private boolean isTransitioning() {
         return getMode() == MODE_AUTO &&
@@ -214,7 +208,7 @@ public class ColorTemperatureController extends LiveDisplayFeature {
             return;
         }
 
-        long duration = (long)(5 * Math.abs(current - balance));
+        long duration = 5L * Math.abs(current - balance);
 
 
         if (DEBUG) {
@@ -230,14 +224,11 @@ public class ColorTemperatureController extends LiveDisplayFeature {
         mAnimator = ValueAnimator.ofInt(current, balance);
         mAnimator.setDuration(duration);
         mAnimator.setInterpolator(mInterpolator);
-        mAnimator.addUpdateListener(new AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(final ValueAnimator animation) {
-                synchronized (ColorTemperatureController.this) {
-                    if (isScreenOn()) {
-                        int value = (int) animation.getAnimatedValue();
-                        mHardware.setColorBalance(value);
-                    }
+        mAnimator.addUpdateListener(animation -> {
+            synchronized (ColorTemperatureController.this) {
+                if (isScreenOn()) {
+                    int value = (int) animation.getAnimatedValue();
+                    mHardware.setColorBalance(value);
                 }
             }
         });
@@ -249,7 +240,8 @@ public class ColorTemperatureController extends LiveDisplayFeature {
      * correct configuration at the device level!
      */
     private int mapColorTemperatureToBalance(int temperature) {
-        double z = com.evervolv.internal.util.MathUtils.powerCurveToLinear(mColorBalanceCurve, temperature);
+        double z = com.evervolv.internal.util.MathUtils.powerCurveToLinear(mColorBalanceCurve,
+                temperature);
         return Math.round(MathUtils.lerp((float)mColorBalanceRange.getLower(),
                 (float)mColorBalanceRange.getUpper(), (float)z));
     }
@@ -296,12 +288,14 @@ public class ColorTemperatureController extends LiveDisplayFeature {
 
         // Scale the transition into night mode in 0.5hr before civil sunset
         if (now <= sunset) {
-            return mInterpolator.getInterpolation((float) (sunset - now) / TWILIGHT_ADJUSTMENT_TIME);
+            return mInterpolator.getInterpolation((float) (sunset - now)
+                    / TWILIGHT_ADJUSTMENT_TIME);
         }
 
         // Scale the transition into day mode in 0.5hr after civil sunrise
         if (now >= sunrise) {
-            return mInterpolator.getInterpolation((float) (now - sunrise) / TWILIGHT_ADJUSTMENT_TIME);
+            return mInterpolator.getInterpolation((float) (now - sunrise)
+                    / TWILIGHT_ADJUSTMENT_TIME);
         }
 
         // More than 0.5hr past civil sunset