CJ001im/android_Sequoia

sdk: Optimize imports
Reordering and removing unused ones

Change-Id: I1fff9c6f11c71022dbf4207ee042d2ee2ff77ee6

lineage/lib/main/java/org/lineageos/platform/internal/display/ColorTemperatureController.java
@@ -5,6 +5,11 @@
  */
 package org.lineageos.platform.internal.display;
 
+import static lineageos.hardware.LiveDisplayManager.MODE_AUTO;
+import static lineageos.hardware.LiveDisplayManager.MODE_DAY;
+import static lineageos.hardware.LiveDisplayManager.MODE_NIGHT;
+import static lineageos.hardware.LiveDisplayManager.MODE_OFF;
+
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.net.Uri;
@@ -24,11 +29,6 @@ import lineageos.hardware.LineageHardwareManager;
 import lineageos.hardware.LiveDisplayManager;
 import lineageos.providers.LineageSettings;
 import lineageos.util.ColorUtils;
-
-import static lineageos.hardware.LiveDisplayManager.MODE_AUTO;
-import static lineageos.hardware.LiveDisplayManager.MODE_DAY;
-import static lineageos.hardware.LiveDisplayManager.MODE_NIGHT;
-import static lineageos.hardware.LiveDisplayManager.MODE_OFF;
 
 public class ColorTemperatureController extends LiveDisplayFeature {
 

lineage/lib/main/java/org/lineageos/platform/internal/display/LiveDisplayFeature.java
@@ -1,9 +1,14 @@
 /*
  * SPDX-FileCopyrightText: 2016 The CyanogenMod Project
- * SPDX-FileCopyrightText: 2017-2019 The LineageOS Project
+ * SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 package org.lineageos.platform.internal.display;
+
+import static org.lineageos.platform.internal.display.LiveDisplayService.ALL_CHANGED;
+import static org.lineageos.platform.internal.display.LiveDisplayService.DISPLAY_CHANGED;
+import static org.lineageos.platform.internal.display.LiveDisplayService.MODE_CHANGED;
+import static org.lineageos.platform.internal.display.LiveDisplayService.TWILIGHT_CHANGED;
 
 import android.content.Context;
 import android.hardware.display.ColorDisplayManager;
@@ -15,11 +20,6 @@ import org.lineageos.platform.internal.display.LiveDisplayService.State;
 import org.lineageos.platform.internal.display.TwilightTracker.TwilightState;
 
 import java.util.BitSet;
-
-import static org.lineageos.platform.internal.display.LiveDisplayService.ALL_CHANGED;
-import static org.lineageos.platform.internal.display.LiveDisplayService.DISPLAY_CHANGED;
-import static org.lineageos.platform.internal.display.LiveDisplayService.MODE_CHANGED;
-import static org.lineageos.platform.internal.display.LiveDisplayService.TWILIGHT_CHANGED;
 
 public abstract class LiveDisplayFeature extends LineageBaseFeature {
 

lineage/lib/main/java/org/lineageos/platform/internal/health/ChargingControlController.java
@@ -6,6 +6,10 @@
 package org.lineageos.platform.internal.health;
 
 import static java.time.format.FormatStyle.SHORT;
+import static lineageos.health.HealthInterface.MODE_AUTO;
+import static lineageos.health.HealthInterface.MODE_LIMIT;
+import static lineageos.health.HealthInterface.MODE_MANUAL;
+import static lineageos.health.HealthInterface.MODE_NONE;
 
 import android.app.AlarmManager;
 import android.app.Notification;
@@ -44,11 +48,6 @@ import lineageos.providers.LineageSettings;
 
 import vendor.lineage.health.ChargingControlSupportedMode;
 import vendor.lineage.health.IChargingControl;
-
-import static lineageos.health.HealthInterface.MODE_NONE;
-import static lineageos.health.HealthInterface.MODE_AUTO;
-import static lineageos.health.HealthInterface.MODE_MANUAL;
-import static lineageos.health.HealthInterface.MODE_LIMIT;
 
 public class ChargingControlController extends LineageHealthFeature {
     private final IChargingControl mChargingControl;

Evervolv/android_vendor_ev_sdk

services/core/java/com/evervolv/platform/internal/display/ColorTemperatureController.java
@@ -5,6 +5,11 @@
  */
 package com.evervolv.platform.internal.display;
 
+import static evervolv.hardware.LiveDisplayManager.MODE_AUTO;
+import static evervolv.hardware.LiveDisplayManager.MODE_DAY;
+import static evervolv.hardware.LiveDisplayManager.MODE_NIGHT;
+import static evervolv.hardware.LiveDisplayManager.MODE_OFF;
+
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.net.Uri;
@@ -17,18 +22,13 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.android.server.twilight.TwilightState;
 
-import java.io.PrintWriter;
-import java.util.BitSet;
-
 import evervolv.hardware.HardwareManager;
 import evervolv.hardware.LiveDisplayManager;
 import evervolv.provider.EVSettings;
 import evervolv.util.ColorUtils;
 
-import static evervolv.hardware.LiveDisplayManager.MODE_AUTO;
-import static evervolv.hardware.LiveDisplayManager.MODE_DAY;
-import static evervolv.hardware.LiveDisplayManager.MODE_NIGHT;
-import static evervolv.hardware.LiveDisplayManager.MODE_OFF;
+import java.io.PrintWriter;
+import java.util.BitSet;
 
 public class ColorTemperatureController extends LiveDisplayFeature {
 

services/core/java/com/evervolv/platform/internal/display/LiveDisplayFeature.java
@@ -1,9 +1,14 @@
 /*
  * SPDX-FileCopyrightText: 2016 The CyanogenMod Project
- * SPDX-FileCopyrightText: 2017-2019 The LineageOS Project
+ * SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.evervolv.platform.internal.display;
+
+import static com.evervolv.platform.internal.display.LiveDisplayService.ALL_CHANGED;
+import static com.evervolv.platform.internal.display.LiveDisplayService.DISPLAY_CHANGED;
+import static com.evervolv.platform.internal.display.LiveDisplayService.MODE_CHANGED;
+import static com.evervolv.platform.internal.display.LiveDisplayService.TWILIGHT_CHANGED;
 
 import android.content.Context;
 import android.hardware.display.ColorDisplayManager;
@@ -14,18 +19,13 @@ import com.android.server.LocalServices;
 import com.android.server.twilight.TwilightManager;
 import com.android.server.twilight.TwilightState;
 
-import java.util.BitSet;
-
 import com.evervolv.platform.internal.VendorBaseFeature;
 import com.evervolv.platform.internal.common.UserContentObserver;
 import com.evervolv.platform.internal.display.LiveDisplayService.State;
 
 import evervolv.provider.EVSettings;
 
-import static com.evervolv.platform.internal.display.LiveDisplayService.ALL_CHANGED;
-import static com.evervolv.platform.internal.display.LiveDisplayService.DISPLAY_CHANGED;
-import static com.evervolv.platform.internal.display.LiveDisplayService.MODE_CHANGED;
-import static com.evervolv.platform.internal.display.LiveDisplayService.TWILIGHT_CHANGED;
+import java.util.BitSet;
 
 public abstract class LiveDisplayFeature extends VendorBaseFeature {
 

services/core/java/com/evervolv/platform/internal/health/ChargingControlController.java
@@ -5,6 +5,11 @@
 
 package com.evervolv.platform.internal.health;
 
+import static evervolv.health.HealthInterface.MODE_NONE;
+import static evervolv.health.HealthInterface.MODE_AUTO;
+import static evervolv.health.HealthInterface.MODE_MANUAL;
+import static evervolv.health.HealthInterface.MODE_LIMIT;
+
 import static java.time.format.FormatStyle.SHORT;
 
 import android.app.AlarmManager;
@@ -29,6 +34,8 @@ import android.util.Log;
 
 import com.evervolv.platform.internal.R;
 
+import evervolv.provider.EVSettings;
+
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
@@ -40,15 +47,8 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
-import evervolv.provider.EVSettings;
-
 import vendor.lineage.health.ChargingControlSupportedMode;
 import vendor.lineage.health.IChargingControl;
-
-import static evervolv.health.HealthInterface.MODE_NONE;
-import static evervolv.health.HealthInterface.MODE_AUTO;
-import static evervolv.health.HealthInterface.MODE_MANUAL;
-import static evervolv.health.HealthInterface.MODE_LIMIT;
 
 public class ChargingControlController extends VendorHealthFeature {
     private final IChargingControl mChargingControl;