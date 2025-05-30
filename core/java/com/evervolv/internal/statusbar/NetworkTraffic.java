2258863619153b37240c4b2058e0b5c3403fe45d
Nick Reuter <nreuter85@gmail.com>
2025-05-29 23:25:56 -0500

    Revert "sdk: Avoid TooManyRequestsException SystemUI crash"
    This reverts commit 71f7e5f2f5f0b3572ccfc2c0c6736596e436e65c.

--- core/java/com/evervolv/internal/statusbar/NetworkTraffic.java
+++ core/java/com/evervolv/internal/statusbar/NetworkTraffic.java
@@ -333,12 +333,7 @@ public class NetworkTraffic extends TextView {
                 }
             };
 
-            try {
-                connectivityManager.registerDefaultNetworkCallback(defaultNetworkCallback);
-            } catch (RuntimeException e) {
-                // ConnectivityManager.TooManyRequestsException
-                Log.e(TAG, "Too many requests made to the ConnectivityManager", e);
-            }
+            connectivityManager.registerDefaultNetworkCallback(defaultNetworkCallback);
         }
     }
 
